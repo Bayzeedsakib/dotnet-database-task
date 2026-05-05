81869131ae66ed4262849f40b7e7115a79c02038
Bayzeedsakib <angelsakib4@gmail.com>
2026-05-05 12:30:53 +0600

    update view and controller

---
+++ DatabaseTask/Controllers/StudentController.cs
@@ -0,0 +1,40 @@
+﻿using DatabaseTask.EF;
+using DatabaseTask.EF.Tables;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatabaseTask.Controllers
+{
+    public class StudentController : Controller
+    {
+        StudentMsBsp26Context db;
+        public StudentController(StudentMsBsp26Context db)
+        {
+            this.db = db;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var data = db.Students.ToList();
+            return View(data);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Student s)
+        {
+            db.Students.Add(s);
+            db.SaveChanges();
+
+            TempData["Msg"] = s.Name + " created successfully";
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
